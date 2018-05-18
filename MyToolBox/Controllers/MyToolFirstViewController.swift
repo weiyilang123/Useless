f65d6ef2bd25c01567114bc3eacc7db27688ffcc
Hans Wei <yilangwei@yilangs-macbook-pro.local>
2018-05-17 21:21:03 -0400

    ToolBox First Commit

--- MyToolBox/Controllers/MyToolFirstViewController.swift
+++ MyToolBox/Controllers/MyToolFirstViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MyToolFirstViewController: UIViewController {
 
+   
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,20 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //MARK: Segue to Weather Page
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        
+        
+        
+    }
+    
+    @IBAction func weatherButton(_ sender: UIButton) {
 
+        performSegue(withIdentifier: "weatherPageSegue", sender: self)
+    }
 
 }
 
